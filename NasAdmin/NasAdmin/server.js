7b717c942b8da4302049534ac871c7d299752dd8
Lensual <lenvisual@gmail.com>
2017-08-11 02:37:22 +0800

    添加配置文件

--- NasAdmin/NasAdmin/server.js
+++ NasAdmin/NasAdmin/server.js
@@ -3,20 +3,30 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+var config = require("./config.json");
+
+//Listen
+var server = app.listen(config.port,config.hostname, function () {
+    console.log("http://%s:%s", server.address().address, server.address().port);
+})
+
 //¶ÁÎÄ¼þ¼Ð
 app.get('/api/readDir', function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     try {
-        res.end(JSON.stringify(fs.readdirSync(req.query.path)));
+        var str = JSON.stringify(fs.readdirSync(req.query.path));
+        res.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' });
+        res.end(str);
     } catch (e) {
-        res.end((JSON.stringify(e)));
+        //log
+        res.writeHead(500, { 'Content-Type': 'text/plain;charset=utf-8' });
+        res.end((JSON.stringify({ code: e.code, message: e.message, stack: e.stack })));
     }
 })
 
 //API INFO
 app.get('/api', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    var str ="";
+    var str = "";
     str += "req.baseUrl: " + req.baseUrl + "\r\n";
     str += "req.body: " + req.body + "\r\n";
     str += "req.params: " + req.params + "\r\n";
@@ -27,20 +37,3 @@ app.get('/api', function (req, res) {
     str += "req.subdomains: " + req.subdomains + "\r\n";
     res.end(str);
 })
-
-//Listen
-var server = app.listen(1337, function () {
-    console.log("http://%s:%s", server.address().address, server.address().port);
-})
-
-
-
-
-
-//var http = require('http');
-//var port = process.env.PORT || 1337;
-
-//http.createServer(function (req, res) {
-//    res.writeHead(200, { 'Content-Type': 'text/plain' });
-//    res.end('Hello World\n');
-//}).listen(port);
