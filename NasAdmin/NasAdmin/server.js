b54dfc66a7c67eb83b2077be1158121ed40a1b44
Lensual <lenvisual@gmail.com>
2017-08-11 14:32:45 +0800

    实现ifErr

--- NasAdmin/NasAdmin/server.js
+++ NasAdmin/NasAdmin/server.js
@@ -1,4 +1,5 @@
 "use strict";
+//require
 var config = require("./config.json");
 var log4js = require("log4js");
 log4js.configure(config.log4js);
@@ -6,6 +7,7 @@ var logger = log4js.getLogger("default");
 var express = require("express");
 var fs = require("fs");
 var app = express();
+//var child_process = require("child_process");
 
 
 //Listen
@@ -16,40 +18,41 @@ var server = app.listen(config.port, config.hostname, function () {
 //读文件夹
 app.get("/api/readDir", function (req, res) {
     fs.readdir(req.query.path, (err, files) => {
-        if (err) {
-            logger.error(err);
-            res.writeHead(500, { "Content-Type": "text/plain;charset=utf-8" });
-            res.end(JSON.stringify({ message: err.message, stack: err.stack }));
-            return;
-        }
+        ifErr(err, res);
         res.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
         res.end(JSON.stringify(files));
     });
 });
 
-//重命名 剪切
+//重命名 剪切 !!注意耗时操作，需要设计异步api
 app.get("/api/rename", function (req, res) {
     fs.rename(req.query.oldPath, req.query.newPath, (err) => {
-        if (err) {
-            logger.error(err);
-            res.writeHead(500, { "Content-Type": "text/plain;charset=utf-8" });
-            res.end(JSON.stringify({ message: err.message, stack: err.stack }));
-            return;
-        }
+        ifErr(err, res);
         res.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
         res.end(JSON.stringify({ message: "success" }));
     });
 });
 
-//复制
-app.get("/api/rename", function (req, res) {
-    fs.rename(req.query.oldPath, req.query.newPath, (err) => {
-        if (err) {
-            logger.error(err);
-            res.writeHead(500, { "Content-Type": "text/plain;charset=utf-8" });
-            res.end(JSON.stringify({ message: err.message, stack: err.stack }));
-            return;
+//复制 !!注意耗时操作，需要设计异步api
+app.get("/api/cp", function (req, res) {
+    fs.stat(req.query.source, (err, stats) => {
+        if (stats.isFile) {
+
+        } else if (stats.isDirectory) {
+
+        } else {
+            //err
         }
+    });
+
+
+
+
+
+
+
+    fs.read(req.query.oldPath, req.query.newPath, (err) => {
+        ifErr(err, res);
         res.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
         res.end(JSON.stringify({ message: "success" }));
     });
@@ -71,3 +74,13 @@ app.get("/api", function (req, res) {
     str += "req.subdomains: " + req.subdomains + "\r\n";
     res.end(str);
 });
+
+//ifErr
+function ifErr(err, res) {
+    if (err) {
+        logger.error(err);
+        res.writeHead(500, { "Content-Type": "text/plain;charset=utf-8" });
+        res.end(JSON.stringify({ message: err.message, stack: err.stack }));
+        return;
+    }
+}
