e407e02d1fb04f4563da123be18e88bbe1031235
Lensual <lenvisual@gmail.com>
2017-08-23 16:57:36 +0800

    完成登录页的跳转

--- webroot/js/function.js
+++ webroot/js/function.js
@@ -1,23 +1,26 @@
 //login
 document.getElementById("loginDialog_btnLogin").onclick = function () {
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = login_xhrOnChange(xhr);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {// 4 = "loaded"
+            if (xhr.status == 200) {// 200 = OK
+                var json = JSON.parse(xhr.responseText);
+                if (json.isSuccess) {
+                    window.location.href = "./manage.html"
+                } else {
+                    alert("Login unsuccessful: " + xhr.status + " " + xhr.responseText);
+                }
+            }
+            else {
+                alert("XHR Error: " + xhr.status + " " + xhr.responseText);
+            }
+        }
+    }
     xhr.open("POST", "http://localhost:1337/api/auth/login", true);     //!!要设计自动获取地址
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.send("user=" + encodeURIComponent(document.getElementById("username").value) + "&" +
         "pwd=" + encodeURIComponent(document.getElementById("password").value));
 }
 
-function login_xhrOnChange(xhr) {
-    if (xhr.readyState == 4) {// 4 = "loaded"
-        if (xhr.status == 200) {// 200 = OK
-            var json = JSON.parse(xhr.responseText);
 
-            //alert(xhr.responseText);
-        }
-        else {
-            alert("Login unsuccessful: " + xhr.status + " " + xhr.responseText);
-        }
-    }
-}
 
