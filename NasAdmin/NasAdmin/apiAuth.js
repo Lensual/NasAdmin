1c78a79b3521c32ff4064bf0fca9b6ea865e3c08
Lensual <lenvisual@gmail.com>
2017-08-27 20:36:49 +0800

    删除oauth相关库，自己写，savecodes

--- NasAdmin/NasAdmin/apiAuth.js
+++ NasAdmin/NasAdmin/apiAuth.js
@@ -3,48 +3,88 @@ var router = express.Router();
 
 var fs = require("fs");
 
+
 //bodyParser
 var bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({ extended: false }));
 
-//OAuthServer
-var OAuthServer = require('express-oauth-server');
-var memorystore = require('./oauthModels.js');
-router.oauth = new OAuthServer({
-    model: memorystore,
-    grants: ['password'],
-    debug: true,
-    useErrorHandler: true,
-    continueMiddleware: true,
-    requireClientAuthentication: { password: false }
-});
-const UnauthorizedRequestError = require('oauth2-server/lib/errors/unauthorized-request-error');
+////OAuthServer
+//var OAuthServer = require('express-oauth-server');
+//var memorystore = require('./oauthModels.js');
+//router.oauth = new OAuthServer({
+//    model: memorystore,
+//    grants: ['password'],
+//    debug: true,
+//    useErrorHandler: true,
+//    continueMiddleware: true,
+//    requireClientAuthentication: { password: false }
+//});
+//const UnauthorizedRequestError = require('oauth2-server/lib/errors/unauthorized-request-error');
+
+const uuidv4 = require('uuid/v4');
 
 //Login
-router.post("/login", function (req, res) {
+router.post("/login", login);
+router.post("/token", login);
+
+function login(req, res) {
+    //check parameter
+    if (grant_type != "password") {
+        global.logger.debug("invalid grant_type: " + getClientIp(req));
+        res.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
+        res.end(JSON.stringify({ isSuccess: false, message: "invalid grant_type" }));
+    } else if (!username) {
+        global.logger.debug("invalid username: " + getClientIp(req));
+        res.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
+        res.end(JSON.stringify({ isSuccess: false, message: "invalid username" }));
+    } else if (!password) {
+        global.logger.debug("invalid password: " + getClientIp(req));
+        res.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
+        res.end(JSON.stringify({ isSuccess: false, message: "invalid password" }));
+    }
+    //query users
     var users = JSON.parse(fs.readFileSync("./users.json").toString());
-    var grant;
+    var grantuser;
     users.forEach(function (user) {
-        if (req.body.user == user.name && req.body.pwd == user.pwd) {
-            grant = user;
+        if (req.body.username == user.username && req.body.password == user.password) {
+            grantuser = user;
         }
     });
-    if (grant != null) {
-        //req.session.user = grant;
-        //global.logger.info("User login successful: \"" + req.session.user.name + "\"," + getClientIp(req));
+    if (grantuser != null) {
+        //generate token
+        grantuser.token = uuidv4();
+        var sessionsStorage = JSON.parse(fs.readFileSync("./sessionStorage.json").toString());
+        for (var session in sessionsStorage) {
+            if (session.username == grantuser.username) {
+                session.tokens.push(grantuser.token);
+            }
+        }
+
+        global.logger.info("User login successful: \"" + grant.username + "\"," + getClientIp(req));
         res.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
-        //res.end(JSON.stringify({ isSuccess: true, message: "success", sessionId: req.sessionID}));
+        res.end(JSON.stringify({ isSuccess: true, message: "success", token: uuidv4() }));
     } else {
-        global.logger.info("User login unsuccessful: \"" + req.body.user + "\"," + getClientIp(req));
+        global.logger.info("User login unsuccessful: \"" + req.body.username + "\"," + getClientIp(req));
         res.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
         res.end(JSON.stringify({ isSuccess: false, message: "invalid username/password" }));
     }
-});
+}
 
 //Logout
 router.get("/logout", function (req, res) {
-    //req.session.unset = 'destroy';
-    //global.logger.info("User logout successful: \"" + req.session.user.name + "\"," + getClientIp(req));
+    //delete token
+    var sessionsStorage = JSON.parse(fs.readFileSync("./sessionStorage.json").toString());
+    for (var session in sessionsStorage) {
+        if (session.username == req.body.username) {
+            for (var i = 0; i < tokens.length; i++) {
+                if (session.tokens[i] == req.query.token) {
+                    session.tokens.splice(i);
+                }
+            }
+        }
+    }
+
+    global.logger.info("User logout successful: \"" + req.body.useusername + "\"," + getClientIp(req));
     res.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
     res.end(JSON.stringify({ isSuccess: true, message: "success" }));
 })
@@ -56,14 +96,6 @@ router.get("/logout", function (req, res) {
 //    res.end(JSON.stringify(req.session));
 //})
 
-
-router.post("/oauth", function (req, res, next) {
-    router.oauth.token()(req, res, next)
-        .then(function (token) {
-            res.locals.oauth = { token: token };
-            next();
-        });
-});
 
 
 //Authenticate
