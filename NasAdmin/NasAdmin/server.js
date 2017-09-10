95973d63c6ab9fd12aaec0f97bbf918d2902e42f
Lensual <lenvisual@gmail.com>
2017-09-11 05:04:18 +0800

    整理server.js，添加404

--- NasAdmin/NasAdmin/server.js
+++ NasAdmin/NasAdmin/server.js
@@ -17,27 +17,23 @@ app.use(function (req, res, next) {
 });
 
 //include module
+//!!需要实现动态
 var apiAuth = require("./apiAuth");
 var apiFs = require("./apiFs");
 app.use("/api/auth", apiAuth);
 app.use("/api/fs", apiFs);
 
-
-
-
 //API INFO
 app.get("/api", function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    var str = "";
-    str += "req.baseUrl: " + req.baseUrl + "\r\n";
-    str += "req.body: " + req.body + "\r\n";
-    str += "req.params: " + req.params + "\r\n";
-    str += "req.originalUrl: " + req.originalUrl + "\r\n";
-    str += "req.path: " + req.path + "\r\n";
-    str += "req.protocol: " + req.protocol + "\r\n";
-    str += "req.query: " + req.query + "\r\n";
-    str += "req.subdomains: " + req.subdomains + "\r\n";
-    res.end(str);
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).end();  //!!待填坑
+});
+
+//404
+app.use(function (req,res) {
+    logger.debug('404 "' + req.originalUrl + '"');
+    res.setHeader('Content-Type', 'application/json');
+    res.status(404).json({ message: "404 Not Found", path: req.originalUrl });
 });
 
 //errorHandler
@@ -45,11 +41,11 @@ app.use(function (err, req, res, next) {
     if (err) {
         logger.error(err);
         if (!res.headersSent) {
-            res.writeHead(500, { "Content-Type": "text/plain;charset=utf-8" });
-            res.end(JSON.stringify({ message: err.message, stack: err.stack }))
+            res.setHeader('Content-Type', 'application/json');
+            res.status(500).json({ message: err.message, stack: err.stack });
         }
     }
-})
+});
 
 //Listen
 //hostname绑定bug
